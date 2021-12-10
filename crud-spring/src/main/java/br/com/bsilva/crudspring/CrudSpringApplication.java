aaaeb242e2f971a6377eb92b155c9a2119bc2417
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-12-10 16:41:17 -0300

    start project

---
+++ crud-spring/src/main/java/br/com/bsilva/crudspring/CrudSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.bsilva.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
+}
---
+++ crud-spring/src/test/java/br/com/bsilva/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bsilva.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
